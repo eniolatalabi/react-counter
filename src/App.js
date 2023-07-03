6ac4cb554b7c4c6356cb719689da9304d7ff8e37
eniolatalabi <talabi.eni@gmail.com>
2023-07-02 18:10:58 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      count : 0
+    }
+  }
+
+    increment = () => {
+      this.setState({count: this.state.count + 1});
+    }
+
+    decrement = () => {
+      this.setState({count: this.state.count - 1});
+    }
+
+  render () {
+    return (
+      <div className="App grid place-content-center h-screen gap-3">
+        <div className='flex gap-1'>
+        <button className='p-3 bg-gray-500 rounded-3xl' onClick={this.increment}>+</button>
+        <button className='p-3 bg-gray-500 rounded-3xl' onClick={this.decrement}>-</button>
+     </div>
+        <h2 className='p-6 bg-gray-500 rounded-3xl grid place-content-center'>{this.state.count}</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
